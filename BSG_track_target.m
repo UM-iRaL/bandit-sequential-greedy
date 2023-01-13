3b2552cfbb633e22225605bf35f2dd550fc63e03
potBagMeat <369230018@qq.com>
2023-01-13 00:21:39 -0500

    Build framework

--- BSG_track_target.m
+++ BSG_track_target.m
@@ -9,3 +9,28 @@ num_robot = 1;
 ACTION_SET = transpose([V(:), W(:)]);
 
 z_d_save = cell(run_len,num_robot,num_rep); % target measurement
+
+% should we get video and image?
+vid = false;
+vis = false;
+
+for rep = 1:num_rep
+    % Setting
+
+    for t = 1:num_iter
+        % Create Robots and Planners
+        for r = 1:num_robot
+            %R(r) = robot_nx();
+            %P(r) = bsg_planner();
+        end
+        % Plan Moves
+
+        % Move Targets
+
+        % Log Mearsurement
+        
+        % Receive Loss -> Update Loss
+       
+    end
+end
+% Plot Measurement
