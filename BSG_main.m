5bf8ed8fb54d49de307cbbec72ca296343e37809
potBagMeat <369230018@qq.com>
2023-01-19 14:49:07 -0500

    rename files and change robots' sensing range to 160m

--- BSG_main.m
+++ BSG_main.m
@@ -2,13 +2,13 @@ clear;
 close;
 % Experiment parameters
 num_rep = 1;
-run_len = 200;
+run_len = 500;
 num_robot = 2;
 num_tg = 2;
 map_size = 70;
 
 % Action set for robots
-[V,W] = meshgrid([3,1],[0, -1, 1, -2, 2]);
+[V,W] = meshgrid([1],[0, -1, 1, -2, 2]);
 ACTION_SET = transpose([V(:), W(:)]);
 
 % Visibility map
@@ -18,8 +18,8 @@ vis_map_save = cell(run_len,num_rep);
 
 % Initial pose for robots
 x_true = zeros(run_len+1, num_robot,3,num_rep); % robots
-x_true(1, 1, :, :) = repmat([0;-20;0],1,num_rep);
-x_true(1, 2, :, :) = repmat([0; 0; pi/4],1,num_rep);
+x_true(1, 1, :, :) = repmat([-30;-30;0],1,num_rep);
+x_true(1, 2, :, :) = repmat([30; 30; pi/4],1,num_rep);
 
 % Initial position for targets
 tg_true = zeros(3,num_tg,run_len+1,num_rep); % dynamic target
@@ -28,7 +28,7 @@ tg_true(:,1,1,:) = repmat([20;0;1],1,num_rep);
 tg_true(:,2,1,:) = repmat([40;0;2],1,num_rep);
 
 % Measurement History Data
-z_d_save = cell(run_len,num_robot,num_rep); % target measurements
+z_d_save = cell(run_len,num_robot,num_rep); % target measurements(range-bearing)
 u_save = zeros(run_len,num_robot,2,num_rep); % control
 
 % Esitimate Data
@@ -46,7 +46,8 @@ for rep = 1:num_rep
     % Create Robots and Planners
     for r = 1:num_robot
         R(r) = robot_nx(x_true(1, r, :, :));
-        %P(r) = bsg_planner();
+        P(r) = bsg_planner_nx_v1(num_robot,r, ACTION_SET, run_len, R(r).T, R(r).r_sense,...
+            R(r).fov,[R(r).r_sigma;R(r).b_sigma]);
     end
     % Visualization
     if viz
@@ -63,6 +64,11 @@ for rep = 1:num_rep
         end
         %h0.xe = draw_traj_nx([],permute(x_save(1,:,:,rep),[1 3 2]),'r:');
         h0.tg_cov = [];
+        h0.tg = [];
+
+        for kk = 1:num_tg
+            h0.tg(kk) = draw_pose_nx([], tg_true(:,kk,1,rep),'r',2.2);
+        end
         title(sprintf('Time Step: %d',0));
         xlabel('x [m]','FontSize',14);
         ylabel('y [m]','FontSize',14);
@@ -72,33 +78,58 @@ for rep = 1:num_rep
         end
     end
 
-    
+    % Sense -> Plan Moves -> Move Targets -> Move Robots -> Log
+    % Measurements -> Compute Loss
     for t = 1:run_len
         % Sense
         for r = 1:num_robot
             % targets
             z_d_save{t, r, rep} = R(r).sense(tg_true(:, :, t, rep)');
         end
-        
+
+        fixed_x0 = cell(num_robot,1); fixed_u = cell(num_robot,1);
         % Plan Moves
         for r = 1:num_robot
-            u_save(t, r, :, rep) = ACTION_SET(:,2);
-            
+            P(r).get_action_prob_dist(t);
+            P(r).next_action_index(t) = discretesample(P(r).action_prob_dist(t,:), 1);
+            u_save(t, r, :, rep) = ACTION_SET(:, P(r).next_action_index(t));
+            %fixed_u{r} = u_save(t, r, :, rep);
         end
+
+        % Move Targets
+        v_tg = 0.25;
+        for kk = 1:num_tg
+            if kk == 1
+                tg_true(:, kk, t+1, rep) = tg_true(:,kk,t,rep) +  [(t - 50 < 0)*v_tg; 0;0] + ...
+                    [0; (t - 50 > 0)*(t - 100 < 0)*v_tg; 0] + [(t - 100 > 0)*(t - 150 < 0)*(-v_tg); 0; 0] + [0; (t - 150 > 0)*(t - 200 < 0)*(-v_tg); 0];
+            elseif kk == 2
+                tg_true(:, kk, t+1, rep) = tg_true(:,kk,t,rep) +  [(t - 50 < 0)*(-v_tg); 0;0] + ...
+                    [0; (t - 50 > 0)*(t - 100 < 0)*v_tg; 0] + [(t - 100 > 0)*(t - 150 < 0)*(v_tg); 0; 0] + [0; (t - 150 > 0)*(t - 200 < 0)*(-v_tg); 0];
+            else
+                %tg_true(:,kk,t+1,rep) = A*tg_true(:,kk,t,rep)+[0.05;0;0] + [chol(W(:,:,kk,t,rep)).'*randn(2,1); 0]; % add Gaussian noise
+            end
+        end
+        
         % Move Robots
+        prev_obj_tg = zeros(num_tg, 1);
         for r = 1:num_robot
             R(r).move(squeeze(u_save(t, r, :, rep)));
             x_true(t+1,r,:,rep) = R(r).get_x();
+            % fixed_x0{r} = x_true(t+1,r,:,rep);
+            % f(a_i|A_{i-1})
+
+            % P(r).loss_after_action
+            obj_tg = P(r).losses_after_action(t, r, squeeze(x_true(t,r,:,rep)), tg_true(:,:, t+1, rep)', squeeze(u_save(t, r, :, rep)), prev_obj_tg);
+            prev_obj_tg = obj_tg;
+            P(r).update_experts(t);
         end
-        % Move Targets
-        for kk = 1:num_tg
-            tg_true(:,kk, t+1, rep) = tg_true(:, kk, t, rep);
-        end
+        
         % Log Mearsurement
             % 1. Maintain a map from robots to detected targets
             % 2. Compute covariance based on map
             
             
+        
         % Key is robot id, Value is a collection of target ids
         target_map = containers.Map('KeyType','double','ValueType','any'); 
         for r = 1:num_robot
@@ -111,6 +142,7 @@ for rep = 1:num_rep
         end
         estm_tg = zeros(2, num_tg);
         estm_tg_cov = zeros(2, 2, num_tg);
+
         % Compute variance
         for r = 1:num_robot
             msrmnt_rb = target_map(r);
@@ -141,7 +173,6 @@ for rep = 1:num_rep
                     estm_tg(:, target_id) = estm_tg_fused;
                     estm_tg_cov(:, :, target_id) = estm_tg_cov_fused;
                 end
-
             end
         end
         
@@ -151,8 +182,8 @@ for rep = 1:num_rep
         tg_cov_save{t, rep} = estm_tg_cov;
         tg_save{t, rep} = estm_tg;
         
-
-            
+        
+        
             
             
         % Receive Loss -> Update Loss
@@ -160,7 +191,9 @@ for rep = 1:num_rep
             % 2. Compute loss
             % 3. Pass loss to bsg_planner()
         
-            
+            % using target at time t+1 to calculate loss function
+        
+        
             
             
             
@@ -181,6 +214,10 @@ for rep = 1:num_rep
             tmp = tg_save{t, rep};
             if ~isempty(tmp)
                 h0.tg_cov = draw_covariances_nx(h0.tg_cov, tmp(:,1:2), tg_cov_save{t,rep},'m');
+            end
+
+            for kk = 1 : num_tg
+                h0.tg(kk) = draw_pose_nx(h0.tg(kk), tg_true(:,kk,t,rep),'r',2.2);
             end
             title(sprintf('Time Step: %d',t));
            
